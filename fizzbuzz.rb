244a1174036d7edefdff36e155cd6d8743a4b330
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-06-15 21:54:07 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,9 @@
 def fizzbuzz(num)
-  if num % 3 == 0
-    "Fizz"
-  end
-  if num % 5 == 0
-    "Buzz"
-  end
   if (num % 3 == 0 && num % 5 == 0)
     "FizzBuzz"
-  end  
+  elsif num % 3 == 0
+    "Fizz"
+  else num % 5 == 0
+    "Buzz"
+  end
 end  
